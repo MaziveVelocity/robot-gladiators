cdeb79efa0da0e97e4c3177b9656778815fa489b
Cayman Gill <cayman.g@hotmail.com>
2021-06-28 19:56:51 -0400

    added multiple rounds and added more detail

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,47 +19,56 @@ for (var i = 0; i < enemyNames.length; i++){
     console.log(enemyNames[i] + " is at " + i + " index")
 }*/
 
-
-window.alert("Welcome to Robot Gladiators!");
-
 var fight = function(enemyName) {
+    
+    while(enemyHealth > 0 && playerHealth > 0){  
+        var promptFight = window.prompt("Would you like to FIGHT or Skip this battle?");
 
-    var promptFight = window.prompt("Would you like to FIGHT or Skip this battle?");
+        if (promptFight === "fight" || promptFight === ""){
+            
+            enemyHealth -= playerAttack;
+            console.log("Enemy Health: " + enemyHealth);
 
-    if (promptFight === "fight" || promptFight === ""){
-        
-        enemyHealth -= playerAttack;
-        console.log("Enemy Health: " + enemyHealth);
+            if (enemyHealth <= 0){
+                window.alert(enemyName + " had died");
+                break;
+            }else{
+                window.alert(enemyName + " still has " + enemyHealth + " health left.");
+            }
 
-        if (enemyHealth <= 0){
-            window.alert(enemyName + " had died")
+            playerHealth -= enemyAttack;
+            console.log("Player Health: " +  playerHealth);
+
+            if (playerHealth <= 0){
+                window.alert(playerName + " had died! You have lost your robot in battle! Game Over!");
+                break;
+            }else{
+                window.alert(playerName + " still has " + playerHealth + " health left.");
+            }
+        }else if (promptFight === "skip" || promptFight === "SKIP"){
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+            
+            if (confirmSkip){
+                window.alert(playerName + " has skip the fight");
+                playerMoney -= 10;
+                console.log("Player's Money: ",playerMoney);
+                break;
+            }else{
+                fight();
+            }
+            
         }else{
-            window.alert(enemyName + " still has " + enemyHealth + " health left.")
+            window.alert("You need to choose a valid option. Try agian!")
         }
-
-        playerHealth -= enemyAttack;
-        console.log("Player Health: " +  playerHealth);
-
-        if (enemyHealth <= 0){
-            window.alert(playerName + " had died")
-        }else{
-            window.alert(playerName + " still has " + playerHealth + " health left.")
-        }
-    }else if (promptFight === "skip" || promptFight === "SKIP"){
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?")
-        
-        if (confirmSkip){
-            window.alert(playerName + " has skip the fight");
-            playerMoney -= 2
-        }else{
-            fight();
-        }
-        
-    }else{
-        window.alert("You need to choose a valid option. Try agian!")
     }
 };
 
 for(var i = 0; i < enemyNames.length; i++){
-    fight(enemyNames[i]);
+    if (playerHealth > 0){
+        window.alert("Welcome to Robot Gladiators! " + (i + 1));
+    }    
+    var pickedEnemy = enemyNames[i];
+    enemyHealth = 50;
+    debugger;
+    fight(pickedEnemy);
 }
